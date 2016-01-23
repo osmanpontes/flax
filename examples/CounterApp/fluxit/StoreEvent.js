61f39213b692554fefbaa321031bdba229592a05
samuelmtimbo <samuelmtimbo@gmail.com>
2016-01-22 23:27:43 -0200

    Created StoreEvent module

---
+++ examples/CounterApp/fluxit/StoreEvent.js
@@ -0,0 +1,15 @@
+var StoreEvent = function (id, name, store) {
+  this.id = id;
+  this.name = name;
+  this.store = store;
+};
+
+StoreEvent.prototype.toString = function () {
+  return this.id.toString();
+};
+
+StoreEvent.prototype.append = function () {
+  return new StoreEvent(`${this.id}/${Array.from(arguments).join('/')}`, this.name, this.store);
+};
+
+export default StoreEvent;
