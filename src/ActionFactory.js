3df297793e3aee6acd84f4024df021ec831187ed
Osman Pontes <osmanpontes@gmail.com>
2016-02-19 21:50:06 -0200

    action chainning

--- src/ActionFactory.js
+++ src/ActionFactory.js
@@ -6,7 +6,11 @@ const ActionFactory = {
       type = `${spec.displayName}/${funcName}`,
       scope = Object.assign(new DispatchCreator(type), spec);
 
-    action = spec[funcName].bind(scope);
+    action = function () {
+      return new Promise((resolve, reject) => {
+        this.apply(Object.assign(scope, {resolve, reject}), arguments);
+      });
+    }.bind(spec[funcName]);
     action.actionType = type;
 
     return action;
